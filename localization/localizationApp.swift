11ff141e4a6d2c8250d0c200bdafc12a07a07e4f
Sherozbek <sherozradjabov.2004@gmail.com>
2023-12-08 18:43:41 +0500

    Initial Commit

---
+++ localization/localizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  localizationApp.swift
+//  localization
+//
+//  Created by Sherozbek on 08/12/23.
+//
+
+import SwiftUI
+
+@main
+struct localizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
