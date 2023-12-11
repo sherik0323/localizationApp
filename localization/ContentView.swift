6106fe7f7d5cf5d7bea557c8103005f4a77f2609
Sherozbek <sherozradjabov.2004@gmail.com>
2023-12-11 16:32:33 +0500

    first

--- localization/ContentView.swift
+++ localization/ContentView.swift
@@ -8,12 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var login = ""
+    @State var pswd = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            
+            VStack(alignment: .leading, spacing: 40){
+                
+                Text("authLabel")
+                    .font(.title)
+                    .fontWeight(.black)
+                
+                VStack(spacing: 20){
+                    VStack(alignment: .leading, spacing: 5){
+                        Text("loginLabel")
+                            .fontWeight(.black)
+                        TextField("", text: $login)
+                            .padding(10)
+                            .background(.white)
+                            .cornerRadius(10)
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 5){
+                        Text("pswdLabel")
+                            .fontWeight(.black)
+                        SecureField("", text: $pswd)
+                            .padding(10)
+                            .background(.white)
+                            .cornerRadius(10)
+                    }
+                    
+                    Button {
+                        //
+                    } label: {
+                        Text("btnLabel")
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(.black)
+                            .foregroundColor(.white)
+                            .cornerRadius(10 )
+                    }
+                }
+                
+                
+            }
+            .padding(10)
+            .background(Color("gray"))
+            .cornerRadius(20)
+            
         }
         .padding()
     }
